a31891c7ec8fa18e92dd801406ed7e77d4a57b40
a2kad <reshetnikovruslan705@gmail.com>
2023-08-28 19:14:45 +0200

    New version, just react

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class Weather extends React.Component{
+  render(){
+    return(
+      <div>1</div>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Weather />
+);
+
+
+
