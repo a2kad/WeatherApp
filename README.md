2799e6105ae160baf074d423a936551be09fb11c
a2kad <reshetnikovruslan705@gmail.com>
2023-09-12 10:26:49 +0200

    Installed TailWind

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather App
+
+Installing of TailWind:
+
+```
+npm i nativewind
+npm i --dev tailwindcss@3.3.2
+```
--- src/screens/WellcomeScreen.js
+++ src/screens/WellcomeScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function WellcomeScreen() {
     return (
-        <View>
+        <View className='flex-1 justify-center items-center'>
             <Text>WellcomeScreen</Text>
         </View>
     )
