9b1222506afefbdcc348267b6082a84b963d4fd8
a2kad <reshetnikovruslan705@gmail.com>
2023-09-16 18:36:27 +0200

    New architecture

--- App.js
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation/index'
-
-export default function App() {
-  return (
-    <AppNavigation />
-  );
-}
